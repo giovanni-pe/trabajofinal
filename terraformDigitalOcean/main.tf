87eae53d1bd97a56da70284939cab38cace1686b
giovanni <gkpe24@gmail.com>
2024-12-20 20:11:02 -0500

    test two

--- terraformDigitalOcean/main.tf
+++ terraformDigitalOcean/main.tf
@@ -34,6 +34,9 @@ resource "digitalocean_droplet" "app_instance_1" {
                 sudo apt install -y git docker-compose
                 git clone https://github.com/giovanni-pe/trabajofinal.git /home/nodeapp
                 cd /home/nodeapp/obs
+                sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                sudo chmod +x /usr/local/bin/docker-compose
+                sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
                 docker-compose up -d
                 EOF
 }
@@ -54,6 +57,9 @@ resource "digitalocean_droplet" "app_instance_2" {
                 sudo apt install -y git docker-compose
                 git clone https://github.com/giovanni-pe/trabajofinal.git /home/nodeapp
                 cd /home/nodeapp/obs
+                sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                sudo chmod +x /usr/local/bin/docker-compose
+                sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
                 docker-compose up -d
                 EOF
 }
